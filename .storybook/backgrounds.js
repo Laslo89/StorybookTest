92ebd4f60d258a3721485e857cfa1fd58f0a1d03
Laslo89 <gohlama56572@th-nuernberg.de>
2018-11-28 08:42:26 +0100

    Create backgrounds.js

---
+++ .storybook/backgrounds.js
@@ -0,0 +1,16 @@
+// Here you can define different backgrounds for your stories.
+export const backgrounds = [
+  {
+    name: 'sidenav',
+    value: '#fff',
+  },
+  {
+    name: 'dark',
+    value: '#58595b',
+  },
+  {
+    name: 'default',
+    value: '#fafafa',
+    default: true,
+  },
+];
