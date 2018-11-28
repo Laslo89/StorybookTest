735464f2d831f19b49f4572d10a1149297688b0b
Laslo89 <gohlama56572@th-nuernberg.de>
2018-11-28 08:43:00 +0100

    Create config.js

---
+++ .storybook/config.js
@@ -0,0 +1,37 @@
+import { configure, addDecorator } from '@storybook/vue';
+import { withBackgrounds } from '@storybook/addon-backgrounds';
+import { backgrounds } from './backgrounds';
+import { withNotes } from '@storybook/addon-notes';
+import './../src/plugins/axios';
+// import i18n, { initI18n } from './../src/i18n/i18n';
+// import './../src/validation/validation';
+import store from './../src/store/store';
+import './../src/vuetify';
+import './../src/styles/styles.scss';
+
+
+
+// initI18n();
+
+addDecorator(withNotes);
+
+addDecorator(
+  withBackgrounds(backgrounds)
+);
+
+addDecorator(() => ({
+  store,
+  template: '<v-app style="background-color: transparent"><story/></v-app>',
+}));
+
+addDecorator(
+  withBackgrounds(backgrounds)
+);
+
+const req = require.context('../src', true, /.story.js$/)
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
